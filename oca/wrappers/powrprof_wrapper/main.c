c0243135847516e31cee17945ebc2c9c79c8321e
skulltrail <samuel_4186@homail.com>
2026-08-09 23:17:32 -0300

    - Making powrprof_wrapper compatible with VIsta

--- oca/wrappers/powrprof_wrapper/main.c
+++ oca/wrappers/powrprof_wrapper/main.c
@@ -107,13 +107,122 @@ typedef enum EFFECTIVE_POWER_MODE
 
 UINT g_LastID = (UINT)-1;
 
+static POWER_PLATFORM_ROLE (WINAPI *pPowerDeterminePlatformRole)(void) = NULL;
+static POWER_PLATFORM_ROLE (WINAPI *pPowerDeterminePlatformRoleEx)(ulong) = NULL;
+static DWORD (WINAPI *pPowerEnumerate)(HKEY,const GUID*,const GUID*,POWER_DATA_ACCESSOR,ULONG,UCHAR*,DWORD*) = NULL;
+static DWORD (WINAPI *pPowerGetActiveScheme)(HKEY,GUID**) = NULL;
+static DWORD (WINAPI *pPowerReadACValue)(HKEY,const GUID*,const GUID*,const GUID*,PULONG,UCHAR*,DWORD*) = NULL;
+static DWORD (WINAPI *pPowerReadDCValue)(HKEY,const GUID*,const GUID*,const GUID*,PULONG,UCHAR*,DWORD*) = NULL;
+static DWORD (WINAPI *pPowerReadACValueIndex)(HKEY,const GUID*,const GUID*,const GUID*,LPDWORD) = NULL;
+static DWORD (WINAPI *pPowerReadDCValueIndex)(HKEY,const GUID*,const GUID*,const GUID*,LPDWORD) = NULL;
+static DWORD (WINAPI *pPowerReadFriendlyName)(HKEY,const GUID*,const GUID*,const GUID*,UCHAR*,DWORD*) = NULL;
+static DWORD (WINAPI *pPowerSetActiveScheme)(HKEY,const GUID*) = NULL;
+static DWORD (WINAPI *pPowerRegisterSuspendResumeNotification)(DWORD, HANDLE, PHPOWERNOTIFY) = NULL;
+static DWORD (WINAPI *pPowerRegisterForEffectivePowerModeNotifications)(ULONG, PVOID, void *, void *) = NULL;
+static DWORD (WINAPI *pPowerUnregisterSuspendResumeNotification)(PHPOWERNOTIFY) = NULL;
+static DWORD (WINAPI *pPowerUnregisterFromEffectivePowerModeNotifications)(PVOID) = NULL;
+static DWORD (WINAPI *pPowerSettingRegisterNotification)(LPCGUID,DWORD,HANDLE,PHPOWERNOTIFY) = NULL;
+static DWORD (WINAPI *pPowerSettingUnregisterNotification)(HPOWERNOTIFY) = NULL;
+static DWORD (WINAPI *pPowerWriteACValueIndex)(HKEY,const GUID*,const GUID*,const GUID*,DWORD) = NULL;
+static DWORD (WINAPI *pPowerWriteDCValueIndex)(HKEY,const GUID*,const GUID*,const GUID*,DWORD) = NULL;
+static DWORD (WINAPI *pPowerWriteSettingAttributes)(const GUID*,const GUID*,DWORD) = NULL;
+
+static HMODULE hPowrProf = NULL;
+
+static void InitPowrProf(void)
+{
+    // if (hPowrProf)
+        // return;
+
+    hPowrProf = GetModuleHandleW(L"powrprofbase.dll");
+    if (!hPowrProf)
+        hPowrProf = LoadLibraryW(L"powrprofbase.dll");
+
+    // if (!hPowrProf)
+        // return;
+
+    pPowerDeterminePlatformRole =
+        (void *)GetProcAddress(hPowrProf, "PowerDeterminePlatformRole");
+		
+    pPowerDeterminePlatformRoleEx =
+        (void *)GetProcAddress(hPowrProf, "PowerDeterminePlatformRoleEx");		
+
+    pPowerEnumerate =
+        (void *)GetProcAddress(hPowrProf, "PowerEnumerate");
+
+    pPowerGetActiveScheme =
+        (void *)GetProcAddress(hPowrProf, "PowerGetActiveScheme");
+
+    pPowerReadACValue =
+        (void *)GetProcAddress(hPowrProf, "PowerReadACValue");
+
+    pPowerReadDCValue =
+        (void *)GetProcAddress(hPowrProf, "PowerReadDCValue");
+
+    pPowerReadACValueIndex =
+        (void *)GetProcAddress(hPowrProf, "PowerReadACValueIndex");
+
+    pPowerReadDCValueIndex =
+        (void *)GetProcAddress(hPowrProf, "PowerReadDCValueIndex");
+
+    pPowerReadFriendlyName =
+        (void *)GetProcAddress(hPowrProf, "PowerReadFriendlyName");
+
+    pPowerSetActiveScheme =
+        (void *)GetProcAddress(hPowrProf, "PowerSetActiveScheme");
+		
+    pPowerRegisterSuspendResumeNotification =
+        (void *)GetProcAddress(hPowrProf, "PowerRegisterSuspendResumeNotification");
+		
+    pPowerRegisterForEffectivePowerModeNotifications =
+        (void *)GetProcAddress(hPowrProf, "PowerRegisterForEffectivePowerModeNotifications");
+
+    pPowerSettingRegisterNotification =
+        (void *)GetProcAddress(hPowrProf, "PowerSettingRegisterNotification");
+
+    pPowerSettingUnregisterNotification =
+        (void *)GetProcAddress(hPowrProf, "PowerSettingUnregisterNotification");
+		
+    pPowerUnregisterSuspendResumeNotification =
+        (void *)GetProcAddress(hPowrProf, "PowerUnregisterSuspendResumeNotification");	
+
+    pPowerUnregisterFromEffectivePowerModeNotifications =
+        (void *)GetProcAddress(hPowrProf, "PowerUnregisterFromEffectivePowerModeNotifications");			
+
+    pPowerWriteACValueIndex =
+        (void *)GetProcAddress(hPowrProf, "PowerWriteACValueIndex");
+
+    pPowerWriteDCValueIndex =
+        (void *)GetProcAddress(hPowrProf, "PowerWriteDCValueIndex");
+
+    pPowerWriteSettingAttributes =
+        (void *)GetProcAddress(hPowrProf, "PowerWriteSettingAttributes");
+}
+
+BOOL WINAPI DllMain(HINSTANCE hInstDLL, DWORD fdwReason, LPVOID lpv)
+{
+    switch(fdwReason)
+    {
+        case DLL_PROCESS_ATTACH:
+            InitPowrProf();
+            break;
+    }
+
+    return TRUE;
+}
+
 DWORD WINAPI
 PowerGetActiveScheme(
 	HKEY UserRootPowerKey,
 	GUID **polguid
 )
 {
-   return ERROR_CALL_NOT_IMPLEMENTED;
+    if(pPowerGetActiveScheme)
+	{
+		return pPowerGetActiveScheme(UserRootPowerKey, polguid);
+	}
+	
+    return ERROR_CALL_NOT_IMPLEMENTED;
 }
 
 DWORD 
@@ -123,7 +232,12 @@ PowerSetActiveScheme(
   _In_     const GUID *SchemeGuid
 )
 {
-   return ERROR_CALL_NOT_IMPLEMENTED;
+    if(pPowerSetActiveScheme)
+	{
+		return PowerSetActiveScheme(UserRootPowerKey, SchemeGuid);
+	}
+	
+    return ERROR_CALL_NOT_IMPLEMENTED;
 }
 
 DWORD 
@@ -138,22 +252,45 @@ PowerReadDCValue(
 	DWORD *BufferSize
 )
 {
-   return ERROR_CALL_NOT_IMPLEMENTED;
+    if(pPowerReadDCValue)
+	{
+		return pPowerReadDCValue(RootPowerKey, 
+		                        Scheme,
+								SubGroup,
+								PowerSettings,
+								Type,
+								Buffer,
+								BufferSize);
+	}
+	
+    return ERROR_CALL_NOT_IMPLEMENTED;
 }
 
 //Need more implementation
-POWER_PLATFORM_ROLE WINAPI 
+POWER_PLATFORM_ROLE 
+WINAPI 
 PowerDeterminePlatformRole(void)
 {
+    if(pPowerDeterminePlatformRole)
+	{
+		return pPowerDeterminePlatformRole();
+	}
+	
 	return PlatformRoleDesktop;
 }
 
 //Need more implementation
-POWER_PLATFORM_ROLE WINAPI 
+POWER_PLATFORM_ROLE 
+WINAPI 
 PowerDeterminePlatformRoleEx(
   _In_ ULONG Version
 )
 {
+    if(pPowerDeterminePlatformRoleEx)
+	{
+		return pPowerDeterminePlatformRoleEx(Version);
+	}
+	
 	return PlatformRoleDesktop;
 }
 
@@ -169,12 +306,38 @@ PowerReadACValue(
   _Inout_opt_       LPDWORD BufferSize
 )
 {
-   return ERROR_CALL_NOT_IMPLEMENTED;
+    if(pPowerReadACValue)
+	{
+		return pPowerReadACValue(RootPowerKey, 
+		                        SchemeGuid,
+								SubGroupOfPowerSettingsGuid,
+								PowerSettingGuid,
+								Type,
+								Buffer,
+								BufferSize);
+	}
+	
+    return ERROR_CALL_NOT_IMPLEMENTED;
 }
 
-DWORD WINAPI PowerSettingRegisterNotification(const GUID *setting, DWORD flags, HANDLE recipient, PHPOWERNOTIFY handle)
+DWORD 
+WINAPI 
+PowerSettingRegisterNotification(
+	const GUID *setting, 
+	DWORD flags, 
+	HANDLE recipient, 
+	PHPOWERNOTIFY handle)
 {
     DbgPrint("(%s,0x%08lx,%p,%p) stub!\n", setting, flags, recipient, handle);
+	
+    if(pPowerSettingRegisterNotification)
+	{
+		return pPowerSettingRegisterNotification(setting, 
+												 flags,
+												 recipient,
+												 handle);
+	}	
+	
     *handle = (PHPOWERNOTIFY)0xdeadbeef;
     return ERROR_SUCCESS;
 }
@@ -182,6 +345,12 @@ DWORD WINAPI PowerSettingRegisterNotification(const GUID *setting, DWORD flags, 
 DWORD WINAPI PowerSettingUnregisterNotification(HPOWERNOTIFY handle)
 {
     DbgPrint("(%p) stub!\n", handle);
+	
+    if(pPowerSettingUnregisterNotification)
+	{
+		return pPowerSettingUnregisterNotification(handle);
+	}		
+	
     return ERROR_SUCCESS;
 }
 
@@ -195,10 +364,20 @@ PowerWriteDCValueIndex(
   _In_           DWORD DefaultDcIndex
 )
 {
-   return ERROR_CALL_NOT_IMPLEMENTED;
+    if(pPowerWriteDCValueIndex)
+	{
+		return pPowerWriteDCValueIndex(RootSystemPowerKey,
+									   SchemePersonalityGuid,
+									   SubGroupOfPowerSettingsGuid,
+									   PowerSettingGuid,
+									   DefaultDcIndex);
+	}	
+    return ERROR_CALL_NOT_IMPLEMENTED;
 }
 
-DWORD WINAPI PowerReadFriendlyName(
+DWORD 
+WINAPI 
+PowerReadFriendlyName(
   _In_opt_        HKEY    RootPowerKey,
   _In_opt_  const GUID    *SchemeGuid,
   _In_opt_  const GUID    *SubGroupOfPowerSettingsGuid,
@@ -207,41 +386,120 @@ DWORD WINAPI PowerReadFriendlyName(
   _Inout_         LPDWORD BufferSize
 )
 {
-   return ERROR_CALL_NOT_IMPLEMENTED;
+    if(pPowerReadFriendlyName)
+	{
+		return pPowerReadFriendlyName(RootPowerKey,
+	                                  SchemeGuid,
+							          SubGroupOfPowerSettingsGuid,
+							          PowerSettingGuid,
+							          Buffer,
+							          BufferSize);
+	}
+	
+	return ERROR_CALL_NOT_IMPLEMENTED;
 }
 
-DWORD WINAPI PowerEnumerate(HKEY key, const GUID *scheme, const GUID *subgroup, POWER_DATA_ACCESSOR flags,
-                        ULONG index, UCHAR *buffer, DWORD *buffer_size)
+DWORD 
+WINAPI 
+PowerEnumerate(
+	HKEY key, 
+	const GUID *scheme, 
+	const GUID *subgroup, 
+	POWER_DATA_ACCESSOR flags,
+    ULONG index, 
+	UCHAR *buffer, 
+	DWORD *buffer_size)
 {
-   return ERROR_CALL_NOT_IMPLEMENTED;
+    if(pPowerEnumerate)
+	{
+		return pPowerEnumerate(key,
+	                           scheme,
+							   subgroup,
+							   flags,
+							   index,
+							   buffer,
+							   buffer_size);
+	}
+	
+	return ERROR_CALL_NOT_IMPLEMENTED;
 }
 
 
-DWORD WINAPI PowerRegisterSuspendResumeNotification(DWORD flags, HANDLE recipient, PHPOWERNOTIFY handle)
+DWORD 
+WINAPI 
+PowerRegisterSuspendResumeNotification(DWORD flags, HANDLE recipient, PHPOWERNOTIFY handle)
 {
     DbgPrint("(0x%08lx,%p,%p) stub!\n", flags, recipient, handle);
+	
+    if(pPowerRegisterSuspendResumeNotification)
+	{
+		return pPowerRegisterSuspendResumeNotification(flags,
+													   recipient,
+													   handle);
+	}	
+	
     *handle = (HPOWERNOTIFY)0xdeadbeef;
     return ERROR_SUCCESS;
 }
 
-DWORD WINAPI PowerUnregisterSuspendResumeNotification(HPOWERNOTIFY handle)
+DWORD 
+WINAPI 
+PowerUnregisterSuspendResumeNotification(
+	HPOWERNOTIFY handle
+	)
 {
     DbgPrint("(%p) stub!\n", handle);
+	
+    if(pPowerUnregisterSuspendResumeNotification)
+	{
+		return pPowerUnregisterSuspendResumeNotification(handle);
+	}		
+
     return ERROR_SUCCESS;
 }
 
-DWORD WINAPI PowerWriteSettingAttributes(const GUID *SubGroupGuid, const GUID *PowerSettingGuid, DWORD Attributes)
+DWORD 
+WINAPI 
+PowerWriteSettingAttributes(
+	const GUID *SubGroupGuid, 
+	const GUID *PowerSettingGuid, 
+	DWORD Attributes)
 {
+    if(pPowerWriteSettingAttributes)
+	{
+		return pPowerWriteSettingAttributes(SubGroupGuid,
+											PowerSettingGuid,
+											Attributes);
+	}	
+	
 	return ERROR_SUCCESS;
 }
 
-DWORD WINAPI PowerWriteACValueIndex(HKEY key, const GUID *scheme, const GUID *subgroup, const GUID *setting, DWORD index)
-{
-   DbgPrint("(%p,%s,%s,%s,0x%08lx) stub!\n", key, (scheme), (subgroup), (setting), index);
-   return ERROR_SUCCESS;
+DWORD 
+WINAPI 
+PowerWriteACValueIndex(
+	HKEY key, 
+	const GUID *scheme, 
+	const GUID *subgroup, 
+	const GUID *setting, 
+	DWORD index)
+{	
+    if(pPowerWriteACValueIndex)
+	{
+		return pPowerWriteACValueIndex(key,
+	                           scheme,
+							   subgroup,
+							   setting,
+							   index);
+	}
+	
+	DbgPrint("(%p,%s,%s,%s,0x%08lx) stub!\n", key, (scheme), (subgroup), (setting), index);	
+	
+	return ERROR_CALL_NOT_IMPLEMENTED;
 }
 
-DWORD 
+DWORD
+WINAPI
 PowerReadACValueIndex(
   HKEY       RootPowerKey,
   const GUID *SchemeGuid,
@@ -250,11 +508,20 @@ PowerReadACValueIndex(
   LPDWORD    AcValueIndex
 )
 {
-   DbgPrint("(%p,%s,%s,%s,0x%08lx) stub!\n", RootPowerKey, (SchemeGuid), (SubGroupOfPowerSettingsGuid), (PowerSettingGuid), AcValueIndex);
-   return ERROR_CALL_NOT_IMPLEMENTED;
+    if(pPowerReadACValueIndex)
+	{
+		return pPowerReadACValueIndex(RootPowerKey,
+	                                  SchemeGuid,
+									  SubGroupOfPowerSettingsGuid,
+							          PowerSettingGuid,
+							          AcValueIndex);
+	}
+	
+	return ERROR_CALL_NOT_IMPLEMENTED;
 }
 
-DWORD 
+DWORD
+WINAPI
 PowerReadDCValueIndex(
 	HKEY       RootPowerKey,
 	const GUID *SchemeGuid,
@@ -262,17 +529,50 @@ PowerReadDCValueIndex(
 	const GUID *PowerSettingGuid,
 	LPDWORD    DcValueIndex
 )
-{
-   DbgPrint("(%p,%s,%s,%s,0x%08lx) stub!\n", RootPowerKey, (SchemeGuid), (SubGroupOfPowerSettingsGuid), (PowerSettingGuid), DcValueIndex);
-   return ERROR_CALL_NOT_IMPLEMENTED;
+{	
+    //DbgPrint("(%p,%s,%s,%s,0x%08lx) stub!\n", RootPowerKey, (SchemeGuid), (SubGroupOfPowerSettingsGuid), (PowerSettingGuid), DcValueIndex);
+    if(pPowerReadDCValueIndex)
+	{
+		return pPowerReadDCValueIndex(RootPowerKey,
+	                                  SchemeGuid,
+									  SubGroupOfPowerSettingsGuid,
+							          PowerSettingGuid,
+							          DcValueIndex);
+	}
+	
+	return ERROR_CALL_NOT_IMPLEMENTED;
 }
 
 // Win 2019 LTSC APIs - required by new Edge on 1809+ modes
-HRESULT WINAPI PowerRegisterForEffectivePowerModeNotifications(ULONG version, PVOID callback, void *context, void **handle)
+HRESULT 
+WINAPI 
+PowerRegisterForEffectivePowerModeNotifications(
+	ULONG version, 
+	PVOID callback, 
+	void *context, 
+	void **handle
+	)
  {
+    if(pPowerRegisterForEffectivePowerModeNotifications)
+	{
+		return pPowerRegisterForEffectivePowerModeNotifications(version,
+																callback,
+																context,
+																handle);
+	}	 
+	 
      return S_OK;
  }
 
-HRESULT WINAPI PowerUnregisterFromEffectivePowerModeNotifications(PVOID RegistrationHandle) {
+HRESULT 
+WINAPI 
+PowerUnregisterFromEffectivePowerModeNotifications(
+	PVOID RegistrationHandle
+	) 
+{
+    if(pPowerUnregisterFromEffectivePowerModeNotifications)
+	{
+		return pPowerUnregisterFromEffectivePowerModeNotifications(RegistrationHandle);
+	}		
     return S_OK;
 }
